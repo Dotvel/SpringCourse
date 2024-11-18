16fc84c00f052a4cb6598617f2f588566e5f0c75
Myasnov Mikhail <myasnovmikhail@gmail.com>
2024-11-18 17:40:57 +0300

    Lessons2.Inversion of Control (IoC)

--- src/main/java/springcourse/TestSpring.java
+++ src/main/java/springcourse/TestSpring.java
@@ -8,8 +8,11 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        TestBean testBean = context.getBean("testBean", TestBean.class);
-        System.out.println(testBean.getName());
+        Music music = context.getBean("musicBean", Music.class);
+
+        MusicPlayer musicPlayer = new MusicPlayer(music); //внедряем зависимость вручную в конструкторе
+        //при создании нового объекта
+        musicPlayer.playMusic();
         context.close();
     }
 }
