577b9711c236680aa3980f59e595645fc039435d
Myasnov Mikhail <myasnovmikhail@gmail.com>
2024-11-27 20:45:38 +0300

    Lessons15.RequestParam and Annotation @RequestParam

--- src/main/java/ru/myasnov/springcourse/config/controllers/FirstController.java
+++ src/main/java/ru/myasnov/springcourse/config/controllers/FirstController.java
@@ -1,8 +1,11 @@
 package ru.myasnov.springcourse.config.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first") //если попробуем переходить по запросу http//..../hello он не найдет эту страницу
@@ -10,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello"; //first это папка в которой будет лежать наш контроллер
     }
 
