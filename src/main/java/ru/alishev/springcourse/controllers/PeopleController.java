f095d5ca759aa9c11d55c8ad6df904e098e9562f
Myasnov Mikhail <myasnovmikhail@gmail.com>
2024-12-23 13:38:43 +0300

    Lessons28.Spring Validator

--- src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
@@ -8,16 +8,19 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.alishev.springcourse.dao.PersonDAO;
 import ru.alishev.springcourse.models.Person;
+import ru.alishev.springcourse.util.PersonValidator;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
     private final PersonDAO personDAO;
+    private final PersonValidator personValidator;
 
     @Autowired
-    public PeopleController(PersonDAO personDAO) {
+    public PeopleController(PersonDAO personDAO, PersonValidator personValidator) {
         this.personDAO = personDAO;
+        this.personValidator = personValidator;
     }
 
     @GetMapping()
@@ -43,6 +46,8 @@ public class PeopleController {
     private String create(@ModelAttribute("person") @Valid Person person,
                           BindingResult bindingResult) { //в этот объект класса BindingResult будут помещаться
         //все ошибки валидации класса Person
+        personValidator.validate(person, bindingResult);
+
         if (bindingResult.hasErrors()) {
             return "people/new";
         }
@@ -59,6 +64,8 @@ public class PeopleController {
     @PostMapping("/{id}")
     public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
                          @PathVariable("id") int id) {
+        personValidator.validate(person, bindingResult);
+
         if (bindingResult.hasErrors()) {
             return "people/edit";
         }
